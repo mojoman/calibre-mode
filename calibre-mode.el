ae607b09f4fbcc12eda5e8f9347b27430d761ca6
Wartan Hachaturow <wart@debian.org>
2020-05-01 21:40:58 +0300

    Add package header and provide

--- calibre-mode.el
+++ calibre-mode.el
@@ -1,3 +1,30 @@
+;;; calibre-mode --- Provide simple interface to calibre database.
+
+;; Author: whacked@github
+;; Keywords: calibre
+;; Version: 0.1
+;; Package-Requires: (org cl sql seq)
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+;; Provides an interface to calibre database.
+
+;;; Code:
+
 (require 'org)
 (require 'cl)
 (require 'sql)
@@ -411,3 +438,7 @@
 (defun org-calibre-link-export (link description format)
   "FIXME: stub function"
   (concat "link in calibre: " link " (" description ")"))
+
+(provide 'calibre-mode)
+
+;;; calibre-mode.el ends here
